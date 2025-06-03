4ab02029b05737b1637c4aed6e7ea7c625daedb7
cheeks <134818917+leftovertoast@users.noreply.github.com>
2025-06-03 21:39:32 +0000

    added tick method and dt variable

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    myclock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +16,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        pygame.tick(60)
+        dt = (pygame.tick() / 1000)
 
 if __name__== "__main__":
     main()
