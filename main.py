67da234ae19ae232b7175048139e66c1079ff565
cheeks <134818917+leftovertoast@users.noreply.github.com>
2025-06-03 21:56:18 +0000

    corrected typo

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-        dt = (myclock.tick(60) / 1000)
+        dt = myclock.tick(60) / 1000
 
 if __name__== "__main__":
     main()
